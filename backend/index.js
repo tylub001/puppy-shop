31f137c9a9f22556aafc001e744c8edb7940141e
Brittany Tylutke <tylub001@gmail.com>
2025-08-21 19:18:22 -0400

    Fix wildcard route for Express 5

--- backend/index.js
+++ backend/index.js
@@ -228,7 +228,7 @@ const path = require("path");
 
 app.use(express.static(path.join(__dirname, "frontend/build")));
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
   res.sendFile(path.join(__dirname, "frontend/build", "index.html"));
 });
 
